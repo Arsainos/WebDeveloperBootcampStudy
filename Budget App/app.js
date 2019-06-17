6b9c2ab7879b12ae414835914ad77c8473ef9f66
Belyavtsev Artyem <arsainos@gmail.com>
2019-06-17 23:02:15 +0300

    function for adding the items in global app controller

--- Budget App/app.js
+++ Budget App/app.js
@@ -15,8 +15,7 @@ var UIController = (function() {
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-    document.querySelector('.add__btn').addEventListener('click', function(){
-        
+    var ctrlAddItem = function() {
         // 1. Get the filed input data
         
         // 2. Add the item to the budget controller
@@ -26,7 +25,14 @@ var controller = (function(budgetCtrl, UICtrl) {
         // 4. Calculate the budget
 
         // 5. Display the budget on the UI
-        
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event){
+        if(event.keyCode === 13 || event.which === 13){
+            ctrlAddItem();
+        }
     });
 
 })(budgetController, UIController);
