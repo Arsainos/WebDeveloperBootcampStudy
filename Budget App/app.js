597c963e4e1debf798870677d7f9143140163179
Belyavtsev Artyem <arsainos@gmail.com>
2019-06-17 22:50:22 +0300

    Add the starter files

---
+++ Budget App/app.js
@@ -0,0 +1,32 @@
+//BUDGET CONTROLLER
+var budgetController = (function() {
+
+    // Some code later
+
+})();
+
+// UI CONTROLLER
+var UIController = (function() {
+
+    // Some code later
+
+})();
+
+// GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl) {
+
+    document.querySelector('.add__btn').addEventListener('click', function(){
+        
+        // 1. Get the filed input data
+        
+        // 2. Add the item to the budget controller
+
+        // 3. Add item to the UI
+
+        // 4. Calculate the budget
+
+        // 5. Display the budget on the UI
+        
+    });
+
+})(budgetController, UIController);
