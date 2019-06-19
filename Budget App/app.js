eafff6c1bc12fcb0df320d96ed92bd9485a0acd8
Belyavtsev Artyem <arsainos@gmail.com>
2019-06-19 21:30:03 +0300

    add function for init and setup function

--- Budget App/app.js
+++ Budget App/app.js
@@ -34,12 +34,21 @@ var UIController = (function() {
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-    var DOM = UICtrl.getDOMsStrings();
+    var setupEventListeners = function() {
+        var DOM = UICtrl.getDOMsStrings();
+
+        document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function(event){
+            if(event.keyCode === 13 || event.which === 13){
+                ctrlAddItem();
+            }
+        });
+    };   
 
     var ctrlAddItem = function() {
         // 1. Get the filed input data
         var input = UICtrl.getInput();
-        console.log(input);
 
         // 2. Add the item to the budget controller
 
@@ -48,14 +57,15 @@ var controller = (function(budgetCtrl, UICtrl) {
         // 4. Calculate the budget
 
         // 5. Display the budget on the UI
+    };
+
+    return {
+        init: function(){
+            console.log('Application has started.');
+            setupEventListeners();
+        }
     }
 
-    document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
-
-    document.addEventListener('keypress', function(event){
-        if(event.keyCode === 13 || event.which === 13){
-            ctrlAddItem();
-        }
-    });
-
 })(budgetController, UIController);
+
+controller.init();
