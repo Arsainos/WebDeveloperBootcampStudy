d643b3aa5027451889cd0c9d8c86bf936ed4a974
Belyavtsev Artyem <arsainos@gmail.com>
2019-06-19 21:23:48 +0300

    Add data read functions and some constants

--- Budget App/app.js
+++ Budget App/app.js
@@ -8,16 +8,39 @@ var budgetController = (function() {
 // UI CONTROLLER
 var UIController = (function() {
 
-    // Some code later
+    var DOMstrings = {
+        inputType : '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputButton: '.add__btn',
+    };
+
+    return {
+        getInput: function() {
+            return {
+            type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exc
+            description: document.querySelector(DOMstrings.inputDescription).value,
+            value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+
+        getDOMsStrings: function(){
+            return DOMstrings;
+        },
+    };
 
 })();
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
+    var DOM = UICtrl.getDOMsStrings();
+
     var ctrlAddItem = function() {
         // 1. Get the filed input data
-        
+        var input = UICtrl.getInput();
+        console.log(input);
+
         // 2. Add the item to the budget controller
 
         // 3. Add item to the UI
@@ -27,7 +50,7 @@ var controller = (function(budgetCtrl, UICtrl) {
         // 5. Display the budget on the UI
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(event){
         if(event.keyCode === 13 || event.which === 13){
