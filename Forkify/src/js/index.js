c0f8d9a7945eaff9f6cdc21a4addc7b6b738f9fa
Belyavtsev Artyem <arsainos@gmail.com>
2019-07-03 00:04:40 +0300

    add functions to the searchView

--- Forkify/src/js/index.js
+++ Forkify/src/js/index.js
@@ -12,19 +12,22 @@ const state = {};
 
 const controlSearch = async () => {
     // 1. Get query from view
-    const query = 'pizza'; //TODO
+    const query = searchView.getInput();
+    console.log(query);
     
     if(query) {
         // 2. New Search object and add to state
         state.search = new Search(query);
 
         // 3. Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
 
         // 4. Search for recipes
         await state.search.getResults();
 
         // 5. render results on UI
-        console.log(state.search.result);
+        searchView.renderResults(state.search.result);
     }
 };
 
--- Forkify/src/js/views/base.js
+++ Forkify/src/js/views/base.js
@@ -1,4 +1,5 @@
 export const elements = {
     searchForm: document.querySelector('.search'),
-    searchInput: document.querySelector('search__field'),
+    searchInput: document.querySelector('.search__field'),
+    saarchResultList: document.querySelector('.results__list'),
 };
